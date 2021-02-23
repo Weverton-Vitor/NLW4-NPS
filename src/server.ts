92fdcd2405ed701fcc41765409e7c12be7913859
weverton-vitor <wevertonvitor01@gmail.com>
2021-02-23 17:56:40 -0300

    First day

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express, { response } from 'express';
+
+const app = express();
+
+/*
+* GET => Buscar
+* POST => Salvar
+* put => Alterar
+* DELETE => Deletar
+* PATACH => Alteração expecifica
+*/
+
+// 1 param => Rota(recurso API)
+// 2 param => resquest, response
+
+app.get("/", (request, response) => {
+    //return response.send('Hello Word - NLW04');
+    return response.json({message:'Hello Word - NLW04'})
+    
+})
+
+app.post("/", (request, response) => {
+    // Recebeu os dados para salvar
+    return response.json({message:"Os dados foram salvos com sucesso!"})
+})
+
+
+app.listen(3333, () => console.log("Server is running!"));
